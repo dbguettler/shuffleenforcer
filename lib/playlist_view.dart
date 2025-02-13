12d2c179b13b61e5b896879625fd22ddd91c85b8
Drew Guettler <69404804+dbguettler@users.noreply.github.com>
2025-02-13 13:41:02 -0500

    Display tracks on playlist page

--- lib/playlist_view.dart
+++ lib/playlist_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shuffle_enforcer/models/playlist.dart';
+import 'package:shuffle_enforcer/models/track.dart';
 
 class PlaylistView extends StatelessWidget {
   const PlaylistView({super.key, required this.playlist});
@@ -8,6 +9,36 @@ class PlaylistView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(body: Center(child: Text(playlist.name))));
+    return SafeArea(
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text(playlist.name),
+            ),
+            body: FutureBuilder(
+              future: playlist.getTracks(),
+              builder: (context, AsyncSnapshot<List<Track>> snapshot) {
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        Track track = snapshot.data![index];
+                        Widget trackWidget = ListTile(
+                            title: Text(track.name),
+                            subtitle: Text(track.artists.join(", ")),
+                            leading: Image.network(track.albumArtUrl));
+
+                        List<Widget> trackAndDivider = index == 0
+                            ? [trackWidget]
+                            : [const Divider(), trackWidget];
+                        return Column(children: trackAndDivider);
+                      });
+                } else if (snapshot.hasError) {
+                  return const Center(
+                      child: Text("Error retrieving tracks in playlist."));
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
+              },
+            )));
   }
 }

a544027f3e31c9674624b4ea1d5d961758ead31a
Drew Guettler <69404804+dbguettler@users.noreply.github.com>
2025-03-03 13:31:07 -0500

    Add required Spotify aattribution

--- lib/playlist_listing.dart
+++ lib/playlist_listing.dart
@@ -29,35 +29,54 @@ class _PlaylistListingState extends State<PlaylistListing> {
           if (snapshot.hasData) {
             List<Playlist> playlists = snapshot.data;
 
-            return ListView.builder(
-              padding: const EdgeInsets.all(8),
-              itemCount: playlists.length,
-              itemBuilder: (BuildContext context, int index) {
-                Playlist playlist = playlists[index];
-                Widget playlistWidget = InkWell(
-                  child: ListTile(
-                      title: Text(playlist.name),
-                      subtitle: Text(playlist.owner ?? "Unavailable"),
-                      leading: playlist.imageUrl != null
-                          ? ClipRRect(
-                              borderRadius: BorderRadius.circular(2),
-                              child: Image.network(playlist.imageUrl!))
-                          : const Icon(Icons.music_note)),
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                PlaylistView(playlist: playlist)));
-                  },
-                );
+            return Column(children: [
+              Padding(
+                  padding: const EdgeInsets.only(
+                      top: 10, bottom: 5, left: 20, right: 20),
+                  child: Row(spacing: 10, children: [
+                    const Text(
+                      "Content provided by:",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    Expanded(
+                        child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 5),
+                            child: Image.asset(
+                                "assets/Spotify_Full_Logo_RGB_Black.png")))
+                  ])),
+              const Divider(),
+              Expanded(
+                  child: ListView.builder(
+                padding: const EdgeInsets.all(8),
+                itemCount: playlists.length,
+                itemBuilder: (BuildContext context, int index) {
+                  Playlist playlist = playlists[index];
+                  Widget playlistWidget = InkWell(
+                    child: ListTile(
+                        title: Text(playlist.name),
+                        subtitle: Text(playlist.owner ?? "Unavailable"),
+                        leading: playlist.imageUrl != null
+                            ? ClipRRect(
+                                borderRadius: BorderRadius.circular(2),
+                                child: Image.network(playlist.imageUrl!))
+                            : const Icon(Icons.music_note)),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  PlaylistView(playlist: playlist)));
+                    },
+                  );
 
-                List<Widget> playlistAndDivider = index == 0
-                    ? [playlistWidget]
-                    : [const Divider(), playlistWidget];
-                return Column(children: playlistAndDivider);
-              },
-            );
+                  List<Widget> playlistAndDivider = index == 0
+                      ? [playlistWidget]
+                      : [const Divider(), playlistWidget];
+                  return Column(children: playlistAndDivider);
+                },
+              ))
+            ]);
           } else if (snapshot.hasError) {
             return const Center(
                 child: Text("Error retrieving playlists or devices."));
